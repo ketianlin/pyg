d4c45b71d4dd30133100aafc600a6073c07a5e50
a-ke <642028114@qq.com>
2020-12-25 11:28:33 +0800

    first

---
+++ application/adminapi/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\adminapi\controller;
+
+use think\Db;
+use tools\jwt\Token;
+
+class Index extends BaseApi
+{
+    public function index()
+    {
+        $token = Token::getToken('fuck');
+        dump($token);
+        $userId = Token::getUserId($token);
+        dump($userId);exit;
+        $goods = Db::table('pyg_goods')->find();
+        $this->ok($goods);
+        return 'adminapi的index的index';
+    }
+}
---
+++ application/route.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+use think\Route;
+
+Route::domain('adminapi', function(){
+    Route::get('/', 'adminapi/index/index');
+//    Route::resource('goods', 'adminapi/goods');
+    Route::get('captcha/:id', '\\think\\captcha\\CaptchaController@index');//访问图片需要
+    //验证码图片
+    Route::get('captcha', 'adminapi/login/captcha');
+    //登录
+    Route::post('login', 'adminapi/login/login');
+    //退出
+    Route::get('logout', 'adminapi/login/logout');
+});
